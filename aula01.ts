f91b8266f341c4879ab89af7d1edf5c354a6c0f8
Kauã Gabriell <kauagabriell64@gmail.com>
2026-01-23 16:24:35 -0300

    :tada: Primeira aula de typescript(Vantagens de usar).

---
+++ aula01.ts
@@ -0,0 +1,14 @@
+/**
+ * Vantagens de usar typescript
+ * 
+ * 1. Tipagem estática
+ * 2. Intellisense
+ * 3. Refatoração
+ * 4. Debug
+ * 5. Melhorias de performance
+ * 6. Melhorias de segurança
+ * 7. Melhorias de produtividade
+ * 8. Melhorias de qualidade
+ * 9. Melhorias de desempenho
+ * 10. Melhorias de escalabilidade
+ */
