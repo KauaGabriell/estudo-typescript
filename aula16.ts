70260dcb970a7d2a96358252befed12cd0d2b842
Kauã Gabriell <kauagabriell64@gmail.com>
2026-01-29 14:09:29 -0300

    Restrigindo Valores.

---
+++ aula16.ts
@@ -0,0 +1,12 @@
+/***Restringindo Valores
+ * Conseguimos restringir valores, para que seja aceito somente os valores passados.
+ */
+
+type StatusGen = 'pending' | 'completed';
+
+let statusValidos: StatusGen;
+statusValidos = 'completed'
+// statusValidos = 'offline' - Erro - Não está atribuindo um valor aceito nas opções.
+
+
+
